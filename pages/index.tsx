9934d6e9ffeaa8ab99a3323a64996e7387a16428
Olusola Akindoju <davidd.akindoju@gmail.com>
2023-07-08 12:58:39 +0100

    minor change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Homepage } from "../components/Homepage/Homepage";
+import { Homepage } from "@/components/Homepage/Homepage";
 import Head from "next/head";
 
 export default function home() {
